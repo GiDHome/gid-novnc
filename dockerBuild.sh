50d3dd7f2651cb52c6baaf9c57417cc493d39dca
gid linux tester <gid@tester.gidhome.com>
2022-08-11 15:55:01 +0200

    comments

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,3 +1,4 @@
-docker build -t gid-novnc --no-cache -f ./Dockerfile .
+docker build --no-cache -t gid-novnc --no-cache -f ./Dockerfile .
+echo "Remember to do this when ready:"
 echo docker tag gid-novnc gidhome/gid-novnc:latest
 echo docker push gidhome/gid-novnc:latest
