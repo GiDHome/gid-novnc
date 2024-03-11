6e5b3ebedb9966ff6f2e5e39364d37f764f454f5
gid linux tester <gid@tester.gidhome.com>
2024-03-11 17:30:09 +0100

    updated to gid 16.1.9d

--- create-basic-preferences.sh
+++ create-basic-preferences.sh
@@ -14,6 +14,8 @@ PREFERENCESFOLDER=$HOME/.gid/${VERSION}/
 mkdir -p ${PREFERENCESFOLDER}
 PREFERENCESFILE=${PREFERENCESFOLDER}gid.ini
 
+echo "preferences files = ${PREFERENCESFILE}"
+
 # with 'SoftwareOpenGL 0' snapshots are black
 # official version preferences file:
 # PREFERENCESFILE=$HOME/.gidDefaults
@@ -24,10 +26,10 @@ echo "Theme_configured 1" >> ${PREFERENCESFILE}
 echo "Theme(Current) GiD_classic_renewed" >> ${PREFERENCESFILE}
 echo "Theme(MenuType) native" >> ${PREFERENCESFILE}
 echo "Theme(HighResolutionScaleFactor) 1" >> ${PREFERENCESFILE}
-echo "OGL_configured 1" >> ${PREFERENCESFILE}
+echo "OpenGL(Configured) 1" >> ${PREFERENCESFILE}
 echo "Theme_configured 1" >> ${PREFERENCESFILE}
 # This is to avoid black screen when doing zoom:
-echo "OGL_emulateFrontBuffer 1" >> ${PREFERENCESFILE}
+echo "OpenGL(EmulateFrontBuffer) 1" >> ${PREFERENCESFILE}
 echo "ShowCheckNewVersion 0" >> ${PREFERENCESFILE}
 # So that gid opens +0- maximized:
 echo "MainWindowGeom 1276x749+0+0" >> ${PREFERENCESFILE}
