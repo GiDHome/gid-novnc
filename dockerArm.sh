e432e4418fb431e29ecc5270ff49b79424a18e33
gid arm aarch64 linux <gid@arm_aws.gidsimulation.com>
2024-10-25 09:39:39 +0000

    mount foler example

--- dockerArm.sh
+++ dockerArm.sh
@@ -2,3 +2,4 @@ docker build --no-cache -t gid-arm-novnc -f Dockerfile.arm .
 docker run -p 8083:8083 -ti gid-arm-novnc
 # docker tag gid-arm-novnc gidhome/gid-arm-novnc:latest gidhome/gid-arm-novnc:17.1.1d
 # docker push gidhome/gid-arm-novnc:latest
+# echo "add ' -v /home/ubuntu/GID-git/Examples-test:/gid/Examples-test ' to mount some examples into docker image."
