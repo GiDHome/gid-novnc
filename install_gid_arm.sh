9132245e34b835696ee2fb9f6a60a050f30d6c75
gid arm aarch64 linux <gid@arm_aws.gidsimulation.com>
2024-10-28 15:52:44 +0000

    gid-novnc arm version updated to gid 17.1.1d

--- install_gid_arm.sh
+++ install_gid_arm.sh
@@ -14,7 +14,7 @@ cd "$gid_path"
 # tar -zxvf gid14.0.2-linux-x64.tar.gz
 # rm gid14.0.2-linux-x64.tar.gz
 # cd gid14.0.2-x64
-VERSION=17.0.1
+VERSION=17.1.1d
 TARFILENAME=gid${VERSION}-linux-arm-x64.tar.xz
 
 # # TARFILENAME is in this same directory
@@ -25,8 +25,8 @@ TARFILENAME=gid${VERSION}-linux-arm-x64.tar.xz
 # wget --progress=dot:mega https://www.gidhome.com/archive/GiD_Developer_Versions/Linux/amd64/${TARFILENAME}
 # www.gidhome.com/archive has been moved (http code 302)
 # adding '-L' so that curl follows the redirection
-# curl -L https://www.gidhome.com/archive/GiD_Developer_Versions/Linux/arm_aarch64/${TARFILENAME} -o ${TARFILENAME}
-curl -L https://www.gidhome.com/archive/GiD_Official_Versions/Linux/arm_aarch64/${TARFILENAME} -o ${TARFILENAME}
+curl -L https://www.gidhome.com/archive/GiD_Developer_Versions/Linux/arm_aarch64/${TARFILENAME} -o ${TARFILENAME}
+# curl -L https://www.gidhome.com/archive/GiD_Official_Versions/Linux/arm_aarch64/${TARFILENAME} -o ${TARFILENAME}
 
 tar -Jxvf ${TARFILENAME}
 rm ${TARFILENAME}
