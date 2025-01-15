69036beb430340de611b75c0b0fd9da027f7be2f
gid linux tester <gid@tester.gidhome.com>
2025-01-15 10:51:26 +0100

    updated to gid 17.1.2d

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,5 +1,6 @@
 docker build --no-cache -t gid-novnc -f ./Dockerfile .
 # echo "Remember to do this when ready:"
-docker tag gid-novnc gidhome/gid-novnc:latest gidhome/gid-novnc:17.1.2d
+docker tag gid-novnc gidhome/gid-novnc:latest 
+docker tag gid-novnc gidhome/gid-novnc:17.1.2d
 docker push gidhome/gid-novnc:latest
 docker push gidhome/gid-novnc:17.1.2d
